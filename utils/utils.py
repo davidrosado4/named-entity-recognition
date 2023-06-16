55c752aaf1d9c7c9fd1234abfa0740e8c3d41c02
davidrosado4 <rosadodav4@gmail.com>
2023-06-16 14:19:52 +0200

    adding bi-lstm

--- utils/utils.py
+++ utils/utils.py
@@ -8,9 +8,11 @@ import seaborn as sns
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import f1_score
 from skseq import sequence_list_c
-#from sklearn import preprocessing
-#from transformers import BertTokenizerFast
-#import tensorflow as tf
+from keras.utils import pad_sequences
+from tensorflow.keras import Model, Input
+from tensorflow import keras
+from tensorflow.keras.layers import InputLayer, Embedding, SpatialDropout1D, Bidirectional, LSTM, Dense, TimeDistributed
+
 
 def get_data_target_sets(data):
     """
@@ -337,107 +339,213 @@ def print_tiny_test_prediction(X, model, tag_dict_rev):
 
         print(prediction + "\n")
 
-# #-----------------------------------------------------------------------------------------------------------------------
-# #----------------------------------- Functions for DL approach. BERT model----------------------------------------------
-# #-----------------------------------------------------------------------------------------------------------------------
-# def process_BERT_data(df):
-#     """
-#     Preprocesses the input dataframe for sentence tagging.
-#
-#     Args:
-#         df (pandas.DataFrame): The input dataframe containing the data to be processed.
-#
-#     Returns:
-#         tuple: A tuple containing the processed data.
-#             - sentences (numpy.ndarray): An array of lists, where each list represents the words in a sentence.
-#             - tag (numpy.ndarray): An array of lists, where each list represents the tags corresponding to the words in a sentence.
-#             - enc_tag (sklearn.preprocessing.LabelEncoder): The fitted LabelEncoder object used to encode the tags.
-#     """
-#     # Fill missing values in the "sentence_id" column with the previous non-null value
-#     df.loc[:, "sentence_id"] = df["sentence_id"].fillna(method="ffill")
-#
-#     # Initialize a LabelEncoder object to encode the "tags" column
-#     enc_tag = preprocessing.LabelEncoder()
-#
-#     # Encode the values in the "tags" column using the LabelEncoder
-#     df.loc[:, "tags"] = enc_tag.fit_transform(df["tags"])
-#
-#     # Group the "words" column by "sentence_id" and convert them into lists
-#     sentences = df.groupby("sentence_id")["words"].apply(list).values
-#
-#     # Group the "tags" column by "sentence_id" and convert them into lists
-#     tag = df.groupby(by='sentence_id')['tags'].apply(list).values
-#
-#     # Return the processed data: sentences, tag, and enc_tag
-#     return sentences, tag, enc_tag
-#
-# def tokenize_BERT(data, max_len=128):
-#     """
-#     Tokenizes the input data using the BERT tokenizer.
-#
-#     Args:
-#         data (list): A list of input sentences or texts to be tokenized.
-#         max_len (int): The maximum length of the tokenized sequences (default: 128).
-#
-#     Returns:
-#         tuple: A tuple containing the tokenized input data.
-#             - input_ids (numpy.ndarray): A 2D array of shape (n_samples, max_len) containing the tokenized input IDs.
-#             - attention_mask (numpy.ndarray): A 2D array of shape (n_samples, max_len) containing the attention masks.
-#     """
-#     # Initialize the BERT tokenizer
-#     tokenizer = BertTokenizerFast.from_pretrained('bert-base-uncased')
-#
-#     input_ids = list()
-#     attention_mask = list()
-#
-#     # Iterate over the data and tokenize each entry
-#     for i in tqdm(range(len(data))):
-#         encoded = tokenizer.encode_plus(
-#             data[i],
-#             add_special_tokens=True,
-#             max_length=max_len,
-#             is_split_into_words=True,
-#             return_attention_mask=True,
-#             padding='max_length',
-#             truncation=True,
-#             return_tensors='np'
-#         )
-#
-#         input_ids.append(encoded['input_ids'])
-#         attention_mask.append(encoded['attention_mask'])
-#
-#     # Convert the lists to numpy arrays and stack them vertically
-#     return np.vstack(input_ids), np.vstack(attention_mask)
-#
-# def create_BERT_model(bert_model, max_len=128):
-#     """
-#     Creates a classification model based on the BERT model.
-#
-#     Args:
-#         bert_model (tf.keras.Model): The BERT model to use as a base.
-#         max_len (int): The maximum length of the input sequences (default: 128).
-#
-#     Returns:
-#         tf.keras.Model: The compiled classification model.
-#     """
-#     input_ids = tf.keras.Input(shape=(max_len,), dtype='int32')
-#     attention_masks = tf.keras.Input(shape=(max_len,), dtype='int32')
-#
-#     # Pass the input_ids and attention_masks to the BERT model
-#     bert_output = bert_model(input_ids, attention_mask=attention_masks, return_dict=True)
-#
-#     # Apply dropout to the last_hidden_state output of BERT
-#     embedding = tf.keras.layers.Dropout(0.3)(bert_output["last_hidden_state"])
-#
-#     # Add a dense layer with softmax activation for classification
-#     output = tf.keras.layers.Dense(17, activation='softmax')(embedding)
-#
-#     # Create the model with inputs and outputs
-#     model = tf.keras.models.Model(inputs=[input_ids, attention_masks], outputs=[output])
-#
-#     # Compile the model with optimizer, loss function, and metrics
-#     model.compile(optimizer=tf.keras.optimizers.Adam(lr=0.00001),
-#                   loss="sparse_categorical_crossentropy",
-#                   metrics=["accuracy"])
-#
-#     return model
+#----------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------- DL Approach ------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------------------------
+def preprocess_BiLSTM_train_data(df, max_len=128):
+    """
+    Preprocesses training data for a BiLSTM model.
+
+    Args:
+        df: DataFrame containing the training data.
+        max_len: Maximum length of sequences (default: 128).
+
+    Returns:
+        X: Preprocessed input data (padded sequences of word indices).
+        y: Preprocessed target data (padded sequences of tag indices).
+        num_words: Total number of unique words in the training data.
+        num_tags: Total number of unique tags in the training data.
+        word2idx: Dictionary mapping words to their corresponding indices.
+        tag2idx: Dictionary mapping tags to their corresponding indices.
+    """
+
+    # Fill missing values in the "sentence_id" column with the previous non-null value
+    df.loc[:, "sentence_id"] = df["sentence_id"].fillna(method="ffill")
+
+    # Define a lambda function to aggregate words and tags into tuples
+    agg_func = lambda s: [(w, t) for w, t in zip(s["words"].values.tolist(),
+                                                 s["tags"].values.tolist())]
+
+    # Group the dataframe by "sentence_id" and apply the aggregation function
+    sentences = df.groupby('sentence_id').apply(agg_func).tolist()
+
+    # Create a list of unique words in the training data and add an "ENDPAD" token
+    words = list(dict.fromkeys(df["words"].values))
+    words.append("ENDPAD")
+    num_words = len(words)
+
+    # Create a list of unique tags in the training data
+    tags = list(dict.fromkeys(df["tags"].values))
+    num_tags = len(tags)
+
+    # Create a word-to-index dictionary
+    word2idx = {w: i + 1 for i, w in enumerate(words)}  # Index starts from 1
+
+    # Create a tag-to-index dictionary
+    tag2idx = {t: i for i, t in enumerate(tags)}
+
+    # Convert words to their corresponding indices using word2idx dictionary
+    X = [[word2idx[w[0]] for w in s] for s in sentences]
+
+    # Pad the sequences of word indices to a fixed length
+    # Use the value num_words-1 for padding
+    X = pad_sequences(maxlen=max_len, sequences=X, padding="post", value=num_words - 1)
+
+    # Convert tags to their corresponding indices using tag2idx dictionary
+    y = [[tag2idx[w[1]] for w in s] for s in sentences]
+
+    # Pad the sequences of tag indices to a fixed length
+    # Use the value of tag2idx["O"] for padding
+    y = pad_sequences(maxlen=max_len, sequences=y, padding="post", value=tag2idx["O"])
+
+    # Return the processed data: X, y, num_words, num_tags, word2idx, tag2idx
+    return X, y, num_words, num_tags, word2idx, tag2idx
+
+
+def preprocess_BiLSTM_test_data(df, word2idx, tag2idx, num_words, max_len=128):
+    """
+    Preprocesses test data for a BiLSTM model.
+
+    Args:
+        df: DataFrame containing the test data.
+        word2idx: Dictionary mapping words to their corresponding indices.
+        tag2idx: Dictionary mapping tags to their corresponding indices.
+        max_len: Maximum length of sequences (default: 128).
+
+    Returns:
+        X: Preprocessed input data (padded sequences of word indices).
+        y: Preprocessed target data (padded sequences of tag indices).
+    """
+
+    # Fill missing values in the "sentence_id" column with the previous non-null value
+    df.loc[:, "sentence_id"] = df["sentence_id"].fillna(method="ffill")
+
+    # Define a lambda function to aggregate words and tags into tuples
+    agg_func = lambda s: [(w, t) for w, t in zip(s["words"].values.tolist(),
+                                                 s["tags"].values.tolist())]
+
+    # Group the dataframe by "sentence_id" and apply the aggregation function
+    sentences = df.groupby('sentence_id').apply(agg_func).tolist()
+
+    # Convert words to their corresponding indices using word2idx dictionary
+    # Ignore words not found in the dictionary (use 0 as the index)
+    X = [[word2idx.get(w[0], 0) for w in s if w[0] in word2idx.keys()] for s in sentences]
+
+    # Pad the sequences of word indices to a fixed length
+    # Use the value num_words-1 for padding
+    X = pad_sequences(maxlen=max_len, sequences=X, padding="post", value=num_words - 1)
+
+    # Convert tags to their corresponding indices using tag2idx dictionary
+    y = [[tag2idx[w[1]] for w in s] for s in sentences]
+
+    # Pad the sequences of tag indices to a fixed length
+    # Use the value of tag2idx["O"] for padding
+    y = pad_sequences(maxlen=max_len, sequences=y, padding="post", value=tag2idx["O"])
+
+    # Return the processed data: X, y
+    return X, y
+
+
+def create_BiLSTM_model(num_words, max_len=128):
+    """
+    Creates a BiLSTM model.
+
+    Args:
+        num_words: Total number of unique words in the input data.
+        max_len: Maximum length of sequences (default: 128).
+
+    Returns:
+        model: BiLSTM model.
+    """
+
+    # Create a sequential model
+    model = keras.Sequential()
+
+    # Add an input layer with the specified input shape
+    model.add(InputLayer((max_len)))
+
+    # Add an embedding layer with the specified input dimension, output dimension, and input length
+    model.add(Embedding(input_dim=num_words, output_dim=max_len, input_length=max_len))
+
+    # Add a spatial dropout layer with a dropout rate of 0.1
+    model.add(SpatialDropout1D(0.1))
+
+    # Add a bidirectional LSTM layer with 100 units, returning sequences, and a recurrent dropout of 0.1
+    model.add(Bidirectional(LSTM(units=100, return_sequences=True, recurrent_dropout=0.1)))
+
+    # Compile the model with an optimizer, loss function, and metrics
+    model.compile(optimizer="adam",
+                  loss="sparse_categorical_crossentropy",
+                  metrics=["accuracy"])
+
+    return model
+def accuracy_lstm(true, pred):
+    """
+    Computes the accuracy of predicted tags compared to true tags, excluding instances where true[i] == 'O'.
+
+    Args:
+        true: A list of true tags.
+        pred: A list of predicted tags.
+
+    Returns:
+        The accuracy score, which measures the proportion of correct predictions.
+    """
+    # Get indexes of those that are not 'O'
+    idx = [i for i, x in enumerate(true) if x != 0]
+
+    # Get the true and predicted tags for those indexes
+    true = [true[i] for i in idx]
+    pred = [pred[i] for i in idx]
+
+    # Use sklearn's accuracy_score to compute the accuracy
+    return accuracy_score(true, pred)
+
+
+def evaluate_lstm(true, pred, tag_dict_rev):
+    """
+    Computes and prints evaluation metrics and displays a confusion matrix based on the true and predicted tags.
+
+    Args:
+        true: A list of true tags.
+        pred: A list of predicted tags.
+        tag_dict_rev: A dictionary mapping tag indexes to tag labels.
+
+    Returns:
+        None
+    """
+    # Compute the accuracy and F1 score using predefined functions
+    acc = accuracy_lstm(true, pred)
+    f1 = f1_score_weighted(true, pred)
+
+    # Print the evaluation results
+    print('Accuracy: {:.4f}'.format(acc))
+    print('F1 Score: {:.4f}'.format(f1))
+
+    # Plot the confusion matrix
+    plot_confusion_matrix(true, pred, tag_dict_rev)
+
+def predict_lstm(model, X):
+    """
+    Predicts labels using an LSTM model.
+
+    Arguments:
+    - model: The LSTM model used for prediction.
+    - X: The input data to be used for prediction.
+
+    Returns:
+    - y_train_pred: The predicted labels based on the input data.
+    """
+
+    # Predict using the LSTM model
+    y_train_pred = model.predict(X)
+
+    # Get the index with the maximum probability as the predicted label
+    y_train_pred = np.argmax(y_train_pred, axis=-1)
+
+    # Limit the predicted labels to a maximum of 16
+    y_train_pred[y_train_pred > 16] = 16
+
+    # Return the predicted labels
+    return y_train_pred
+
+
